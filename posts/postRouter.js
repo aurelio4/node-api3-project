f5b83819ea552b408beaaadb9559f4b86fd0253f
Aurelio Arcabascio <aurelioarcabascio@gmail.com>
2020-05-06 23:28:27 -0400

    finished project

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,74 @@
 const express = require('express');
+const db = require('./postDb')
 
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  // do your magic!
+// GET: get all posts
+router.get('/', async (req, res) => {
+  try {
+    const users = await db.get()
+    res.status(200).json(users)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Couldn't fulfill request" })
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+// GET: get post by ID
+router.get('/:id', validatePostId, async (req, res) => {
+  try {
+    const id = req.params.id
+    const postById = await db.getById(id)
+    res.status(200).json(postById)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Couldn't fulfill request" })
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+// DELETE: delete post by ID
+router.delete('/:id', validatePostId, async (req, res) => {
+  try {
+    const id = req.params.id
+    await db.remove(id)
+    res.status(200).json({ success: "Successfully deleted" })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Couldn't fulfill request" })
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+// PUT: update post by ID
+router.put('/:id', validatePostId, async (req, res) => {
+  try { 
+      const id = req.params.id
+      const postToUpdate = await db.getById(id)
+      const newPost = {
+        ...postToUpdate,
+        text: req.body.text
+      }
+      await db.update(id, newPost)
+      res.status(200).json(newPost)
+  } catch (err) {
+      console.error(err)
+      res.status(500).json({ error: "Couldn't fulfill request" })
+  }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
+async function validatePostId(req, res, next) {
+  try {
+    const id = req.params.id
+    const postById = await db.getById(id)
+    if(!postById) {
+      res.status(400).json({ error: `Couldn't find post with id:${id}` })
+    } else {
+      next()
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Couldn't fulfill request" })
+  }
 }
 
 module.exports = router;
